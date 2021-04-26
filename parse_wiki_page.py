82da016777ffdfc8ff43d8b9cf0f81d7ad601456
adamhochberger <adamhochberger@gmail.com>
2021-04-25 20:57:28 -0400

    Change parser to use class structure with dummy functions

--- parse_wiki_page.py
+++ parse_wiki_page.py
@@ -3,19 +3,42 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def parse_anchors(url):    
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.content, 'html.parser')
+class Parser:
+    the_soup = None
 
-    # Retrieve all of the anchor tags
-    tags = soup.find_all('a')
-    for tag in tags:
-        print(tag.get('href', None)) 
+    def __init__(self, url):
+        self.make_the_soup(url)
+
+    def make_the_soup(self, url):
+        resp = requests.get(url)
+        soup = BeautifulSoup(resp.content, 'html.parser')
+
+        self.the_soup = soup
+
+    def remake_the_soup(self, url):
+        self.make_the_soup(url)
+
+    def parse_anchors(self):
+
+        # Retrieve all of the anchor tags
+        tags = self.the_soup.find_all('a')
+        for tag in tags:
+            print(tag.get('href', None))
+
+    def parse_ability(self):
+        pass
+
+    def parse_stats(self):
+        pass
+
+    def parse_item(self):
+        pass
 
 
 def main():
     url = "https://leagueoflegends.fandom.com/wiki/Annie/LoL"
-    parse_anchors(url)
+    url_parser = Parser(url)
+    url_parser.parse_anchors()
 
 
 if __name__ == "__main__":

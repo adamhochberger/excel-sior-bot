2e55c4f0525e78410226b749f3ba4362e8ab34e1
adamhochberger <adamhochberger@gmail.com>
2021-07-23 23:04:24 -0400

    Create slash command and split long string function

--- bot.py
+++ bot.py
@@ -1,11 +1,33 @@
 # bot.py
 import os
-from parse_wiki_page import Parser
-from UniteParser import UniteParser
+# from parse_wiki_page import Parser
+from UniteParser import UniteParser, INFO_LIST, ITEMS_LIST
 
 from discord import Client, Intents, Embed
 from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_option, create_choice
 from dotenv import load_dotenv
+
+def split_string(table_string):
+    n = 1994
+    if len(table_string) > n:
+        chunks = []
+        i = 0
+        while True:
+            if len(table_string) - i < n:
+                chunk = table_string[i:len(table_string)]
+                chunks.append(chunk)
+                break
+            else:
+                newline_position = table_string.rfind("\n", i, i+n)            
+                newline_position = newline_position+1
+                chunk = table_string[i:newline_position]
+                i = newline_position
+                chunks.append(chunk)
+        
+        return chunks
+    else:
+        return [table_string]    
 
 load_dotenv()
 
@@ -19,13 +41,25 @@ slash = SlashCommand(bot, sync_commands=True)
 async def on_ready():
     print("Ready!")
 
-@slash.slash(name="unite_item",  guild_ids=GUILD)
-async def _unite_item(ctx: SlashContext):
-    # name = ctx.args[0]
-    # parsed_string = UniteParser(name).csv_parse()
-    # await ctx.send(parsed_string)
+@slash.slash(name="unite_item", 
+    description="Allows you to check information about an item from Pokemon Unite", 
+    guild_ids=GUILD,
+    options=[
+        create_option(
+            name="item_name",
+            description="This is the item that you want to view stats on.",
+            option_type=3,
+            required=True,
+            choices=[create_choice(name=item, value=item) for item in ITEMS_LIST]
+        )
+    ]
+)
+async def _unite_item(ctx: SlashContext, item_name: str):
+    parsed_string = UniteParser(item_name).csv_parse()
 
-    await ctx.send("Test")
+    for chunk in split_string(parsed_string):
+        await ctx.send("```" + chunk + "```")
+    
 
 @slash.slash(name="ping", guild_ids=GUILD)
 async def _ping(ctx): # Defines a new "context" (ctx) command called "ping."

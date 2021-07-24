c34f58fd0a35e2cdaf619f01398ad2e3e158dfcb
adamhochberger <adamhochberger@gmail.com>
2021-07-24 00:12:57 -0400

    Remove name main section from UniteParser

--- UniteParser.py
+++ UniteParser.py
@@ -2,7 +2,7 @@ import csv
 from tabulate import tabulate
 from os import read
 
-ITEMS_LIST = ['Muscle Band', 'Scope Lens', 'Shell Bell', 'WiseGlasses', 'Focus Band', 'Energy Amplifier', 'Float Stone', 'Buddy Barrier', 'Score Shield', 'Aeos Cookie', 'Attack Weight', 'Sp Atk Specs', 'Leftovers', 'Assault Vest', 'Rocky Helmet']
+ITEMS_LIST = ['Muscle Band', 'Scope Lens', 'Shell Bell', 'Wise Glasses', 'Focus Band', 'Energy Amplifier', 'Float Stone', 'Buddy Barrier', 'Score Shield', 'Aeos Cookie', 'Attack Weight', 'Sp Atk Specs', 'Leftovers', 'Assault Vest', 'Rocky Helmet']
 INFO_LIST =  ['Levels', 'Enhancers', 'Dollars / Level']
 
 class UniteParser():
@@ -35,8 +35,4 @@ class UniteParser():
         item_table_string = self.item_to_print + "\n" + tabulate(constants_with_item, headers="keys", tablefmt="pretty")
         # print(item_table_string)
         return item_table_string
-    
-    
-
-if __name__ == "__main__":
-    UniteParser("WiseGlasses").csv_parse()
+        

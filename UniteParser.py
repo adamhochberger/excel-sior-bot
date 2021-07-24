d297be8fca9bb3bcdbfd47de8a8ec4758621c08b
adamhochberger <adamhochberger@gmail.com>
2021-07-23 21:10:49 -0400

    Add logic for parsing csv and transposing

--- UniteParser.py
+++ UniteParser.py
@@ -11,7 +11,12 @@ class UniteParser():
         self.item_to_print = item
 
     def csv_parse(self):
-        pass
-    
+        with open('stat_sheet.csv', 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            big_list = []
+            for row in reader:
+                big_list.append(row)
+
+        transposed_csv = zip(*big_list)
 if __name__ == "__main__":
     UniteParser("WiseGlasses").csv_parse()

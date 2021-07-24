ba99d26360c70b8d94cc38992770236936879be7
adamhochberger <adamhochberger@gmail.com>
2021-07-23 23:00:37 -0400

    Move info and item constants out of class

--- UniteParser.py
+++ UniteParser.py
@@ -2,13 +2,13 @@ import csv
 from tabulate import tabulate
 from os import read
 
+ITEMS_LIST = ['Muscle Band', 'Scope Lens', 'Shell Bell', 'WiseGlasses', 'Focus Band', 'Energy Amplifier', 'Float Stone', 'Buddy Barrier', 'Score Shield', 'Aeos Cookie', 'Attack Weight', 'Sp Atk Specs', 'Leftovers', 'Assault Vest', 'Rocky Helmet']
+INFO_LIST =  ['Levels', 'Enhancers', 'Dollars / Level']
+
 class UniteParser():
-
+    
     def __init__(self, item):
-        self.INFO_LIST =  ['Levels', 'Enhancers', 'Dollars / Level']
-        #self.ITEMS_LIST = ['Muscle Band', 'Scope Lens', 'Shell Bell', 'WiseGlasses', 'Focus Band', 'Energy Amplifier', 'Float Stone', 'Buddy Barrier', 'Score Shield', 'Aeos Cookie', 'Attack Weight', 'Sp Atk Specs', 'Leftovers', 'Assault Vest', 'Rocky Helmet']
-
-        self.CONSTANTS_LIST = self.INFO_LIST + [item]
+        self.CONSTANTS_LIST = INFO_LIST + [item]
         self.item_dictionary = {key: {} for key in self.CONSTANTS_LIST}
         self.item_to_print = item
 
@@ -28,12 +28,15 @@ class UniteParser():
                     key_string = key_string.replace(constant + " ", "")
                     self.item_dictionary[constant][key_string] = row[1:]
         
-        constants_with_item = {key: self.item_dictionary[key][key] for key in self.INFO_LIST}
+        constants_with_item = {key: self.item_dictionary[key][key] for key in INFO_LIST}
         for key, value in self.item_dictionary[self.item_to_print].items():
             constants_with_item[key] = value
 
         item_table_string = self.item_to_print + "\n" + tabulate(constants_with_item, headers="keys", tablefmt="pretty")
-        print(item_table_string)
+        # print(item_table_string)
+        return item_table_string
+    
+    
 
 if __name__ == "__main__":
     UniteParser("WiseGlasses").csv_parse()

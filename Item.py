64a491bd5d8384db3138fda0f3f4e8f085deb344
adamhochberger <adamhochberger@gmail.com>
2021-07-24 02:51:36 -0400

    Initial Item enum class

---
+++ Item.py
@@ -0,0 +1,18 @@
+import enum
+
+class Items(enum.Enum):
+    MUSCLE_BAND = "Muscle Band"
+    SCOPE_LENS = 'Scope Lens'
+    SHELL_BELL = 'Shell Bell'
+    WISE_GLASSES = 'Wise Glasses'
+    FOCUS_BAND = 'Focus Band'
+    ENERGY_AMPLIFIER = 'Energy Amplifier'
+    FLOAT_STONE = 'Float Stone'
+    BUDDY_BARRIER = 'Buddy Barrier'
+    SCORE_SHIELD = 'Score Shield'
+    AEOS_COOKIE = 'Aeos Cookie'
+    ATTACK_WEIGHT = 'Attack Weight'
+    SP_ATK_SPECS = 'Sp Atk Specs'
+    LEFTOVERS = 'Leftovers'
+    ASSAULT_VEST = 'Assault Vest'
+    ROCKY_HELMET = 'Rocky Helmet'

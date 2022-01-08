c1c7b5b22ddf395df5e6e4b7b683222f6857e67a
adamhochberger <adamhochberger@gmail.com>
2022-01-08 09:18:26 -0500

    Add command to to return datetime when resin is capped out
    Add a simple command that gets the remaining resin value left, and creates a timedelta from the current datetime to obtain the time when resin would cap out

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 # bot.py
 import os
+
+from datetime import datetime, timedelta
 from discord import Client, Intents
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_commands import create_option, create_choice
@@ -17,9 +19,11 @@ GUILD = [int(os.getenv('DISCORD_GUILD'))]
 bot = Client(intents=Intents.default())
 slash = SlashCommand(bot, sync_commands=True)
 
+
 @bot.event
 async def on_ready():
     print("Ready!")
+
 
 @slash.slash(
     name="unite_item", 
@@ -52,7 +56,31 @@ async def _unite_item(ctx: SlashContext, item_name: str):
     
 
 @slash.slash(name="ping", guild_ids=GUILD)
-async def _ping(ctx): 
+async def _ping(ctx: SlashContext):
     await ctx.send(f"Pong! ({bot.latency*1000}ms)")
+
+
+@slash.slash(
+    name="resin_timer",
+    description="Allows you to check when your Original Resin resource in Genshin Impact would be fully refreshed",
+    guild_ids=GUILD,
+    options=[
+        create_option(
+            name="current_resin_value",
+            description="This is how much resin you currently have available",
+            option_type=4,
+            required=True
+        )
+    ]
+)
+async def _resin_time(ctx: SlashContext, current_resin_value: int):
+    resin_remaining = 160 - current_resin_value
+    minutes_remaining = 10 * resin_remaining
+    current_time = datetime.now()
+
+    time_with_full_resin = current_time + timedelta(minutes=minutes_remaining)
+
+    formatted_final_time = time_with_full_resin.strftime("%Y-%m-%d %l:%M %p")
+    await ctx.send(f"Your Original Resin ({current_resin_value}/160) will be fully refreshed at {formatted_final_time}")
 
 bot.run(TOKEN)

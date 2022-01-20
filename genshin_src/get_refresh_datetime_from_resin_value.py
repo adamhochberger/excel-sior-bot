0dd5d831bae4eefadfa73d63e0e1ae808a82a8af
adamhochberger <adamhochberger@gmail.com>
2022-01-20 12:23:29 -0500

    Adjust datetime string to be simpler

--- genshin_src/get_refresh_datetime_from_resin_value.py
+++ genshin_src/get_refresh_datetime_from_resin_value.py
@@ -10,7 +10,7 @@ def get_refresh_datetime_from_resin_value(current_resin_value: int) -> str:
     current_time = datetime.now()
 
     time_with_full_resin = current_time + timedelta(minutes=minutes_remaining)
-    formatted_final_time = time_with_full_resin.strftime("%Y-%m-%d %l:%M %p")
+    formatted_final_time = time_with_full_resin.strftime("%D %r")
     formatted_return_string = f"Your Original Resin ({current_resin_value}/{MAX_RESIN_VALUE}) will be fully refreshed at {formatted_final_time}"
 
     return formatted_return_string

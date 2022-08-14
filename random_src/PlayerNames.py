3a956c17b4c0c0caa00a717c28742fe315a95eee
adamhochberger <adamhochberger@gmail.com>
2022-08-13 23:11:49 -0400

    Add new directory and function

---
+++ random_src/PlayerNames.py
@@ -0,0 +1,11 @@
+from BaseEnum import BaseEnum
+
+
+class PlayerNames(BaseEnum):
+    ADAM = "Adam"
+    ANNAN = "Annan"
+    BRADY = "Brady"
+    GIANCARLO = "Giancarlo"
+    JONATHAN = "Jonathan"
+    KYLE = "Kyle"
+    MICHAEL = "Michael"
---
+++ random_src/generate_role_for_player.py
@@ -0,0 +1,5 @@
+from typing import List
+
+
+def generate_role_for_player(player_names: List[str]) -> str:
+    return player_names

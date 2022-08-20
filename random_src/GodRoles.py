1f5e43bffbbbeda99784deaf34f5cd2f60ac722a
adamhochberger <adamhochberger@gmail.com>
2022-08-20 16:44:31 -0400

    Add dictionary of gods by role

---
+++ random_src/GodRoles.py
@@ -0,0 +1,27 @@
+
+GOD_ROLE_DICTIONARY = {
+    "ASSASSIN": [
+        'Arachne', 'Awilix', 'Bakasura', 'Bastet', 'Camazotz', 'Cliodhna', 'Da Ji', 'Fenrir', 'Hun Batz', 'Kali',
+        'Lancelot', 'Loki', 'Mercury', 'Ne Zha', 'Nemesis', 'Pele', 'Ratatoskr', 'Ravana', 'Serqet', 'Set', 'Susano',
+        'Thanatos', 'Thor', 'Tsukuyomi'
+    ],
+    "GUARDIAN": [
+        'Ares', 'Artio', 'Athena', 'Atlas', 'Bacchus', 'Cabrakan', 'Cerberus', 'Cthulhu', 'Fafnir', 'Ganesha', 'Geb',
+        'Jormungandr', 'Khepri', 'Kumbhakarna', 'Kuzenbo', 'Sobek', 'Sylvanus', 'Terra', 'Xing Tian', 'Yemoja', 'Ymir'
+    ],
+    "HUNTER": [
+        'Ah Muzen Cab', 'Anhur', 'Apollo', 'Artemis', 'Cernunnos', 'Charybdis', 'Chernobog', 'Chiron', 'Cupid',
+        'Danzaburou', 'Hachiman', 'Heimdallr', 'Hou Yi', 'Ishtar', 'Izanami', 'Jing Wei', 'Medusa', 'Neith', 'Rama',
+        'Skadi', 'Ullr'
+    ],
+    "MAGE": [
+        'Agni', 'Ah Puch', 'Anubis', 'Ao Kuang', 'Aphrodite', 'Baba Yaga', 'Baron Samedi', "Chang'e", 'Chronos',
+        'Discordia', 'Eset', 'Freya', 'Hades', 'He Bo', 'Hel', 'Hera', 'Janus', 'Kukulkan', 'Merlin', 'Morgan Le Fay',
+        'Nox', 'Nu Wa', 'Olorun', 'Persephone', 'Poseidon', 'Ra', 'Raijin', 'Scylla', 'Sol', 'The Morrigan', 'Thoth',
+        'Tiamat', 'Vulcan', 'Yu Huang', 'Zeus', 'Zhong Kui'
+    ],
+    "WARRIOR": [
+        'Achilles', 'Amaterasu', 'Bellona', 'Chaac', 'Cu Chulainn', 'Erlang Shen', 'Gilgamesh', 'Guan Yu', 'Hercules',
+        'Horus', 'King Arthur', 'Mulan', 'Nike', 'Odin', 'Osiris', 'Shiva', 'Sun Wukong', 'Tyr', 'Vamana'
+    ]
+}
